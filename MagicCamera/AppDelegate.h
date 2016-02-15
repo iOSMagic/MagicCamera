a8ff3722fcfdb1d52e1a51362f7b3e59b25ac026
songwentong <songwentong@mobimagic.com>
2016-02-15 10:36:57 +0800

    创建项目

---
+++ MagicCamera/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MagicCamera
+//
+//  Created by SongWentong on 2/15/16.
+//  Copyright © 2016 SongWentong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MagicCamera/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MagicCamera
+//
+//  Created by SongWentong on 2/15/16.
+//  Copyright © 2016 SongWentong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
