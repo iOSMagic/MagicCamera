d1fc9ec066aa98f3f036f856f6b0ac9bf49552d6
songwentong <songwentong@mobimagic.com>
2016-02-15 15:10:30 +0800

    编辑页

---
+++ MagicCamera/EditImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditImageViewController.h
+//  MagicCamera
+//
+//  Created by SongWentong on 2/15/16.
+//  Copyright © 2016 SongWentong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditImageViewController : UIViewController
+@property (nonatomic,strong) UIImage *image;
+@end
