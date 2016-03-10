ab6b89562ba37d00bd29a1f376c8bef8198a48d6
lipengpeng <lipengpeng@mobimagic.com>
2016-03-10 11:49:19 +0800

    视频处理
    视频处理

--- MagicCamera/ViewController.m
+++ MagicCamera/ViewController.m
@@ -13,6 +13,7 @@
 #import "UIViewController+IBHelper.h"
 #import "CIEnhanceViewController.h"
 #import "VideoCaptureVC.h"
+#import "AVSEViewController.h"
 @interface ViewController () <UITableViewDataSource,UITableViewDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
 {
     
@@ -141,7 +142,7 @@
             break;
         case 2:
         {
-            number = 1;
+            number = 2;
         }
             break;
             
@@ -172,7 +173,7 @@
             break;
         case 2:
         {
-            cell.textLabel.text = @[@"录像"][indexPath.row];
+            cell.textLabel.text = @[@"录像",@"视频处理"][indexPath.row];
         }
             break;
             
@@ -233,8 +234,17 @@
             break;
         case 2:
         {
-            VideoCaptureVC *vc = [VideoCaptureVC instanceFromIB];
-            [self.navigationController pushViewController:vc animated:YES];
+            if (indexPath.row==0)
+            {
+                VideoCaptureVC *vc = [VideoCaptureVC instanceFromIB];
+                [self.navigationController pushViewController:vc animated:YES];
+                
+            }
+            else if (indexPath.row==1)
+            {
+                AVSEViewController *vc = [[AVSEViewController alloc] initWithNibName:@"AVSEViewController_iPad" bundle:nil];;
+                [self.navigationController pushViewController:vc animated:YES];
+            }
         }
             break;
             
